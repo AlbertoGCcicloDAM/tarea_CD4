747cfbe00e689e432cb7b55f8e5b25101e7948ef
albertogcciclodam@gmail.com <>
2024-03-04 15:41:57 +0100

    commit refactor y sonarlint

---
+++ src/banco/MyOwnRuntimeException.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package banco;
+
+/**
+ *
+ * @author VSPC-BLACKFRIDAY
+ */
+public class MyOwnRuntimeException extends Exception {
+
+    public MyOwnRuntimeException(String n) {
+    }
+    
+}
